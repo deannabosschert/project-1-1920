76b62930d092733654df265b35b528ac17174cb9
deannabosschert <deanna.bosschert@gmail.com>
2020-03-02 12:25:46 +0100

    add render-module

---
+++ src/js/modules/render.js
@@ -0,0 +1,21 @@
+function render(data) {
+  const main = document.querySelector('main');
+  const results = data.results;
+  console.dir(results);
+  results.map((item, i) => {
+    const html = `
+            <article>
+              <h2>${item.titles[0]}</h2>
+              <p>${item.summaries ? item.summaries[0] : 'Geen samenvatting'}</p>
+              <img src="${
+                item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
+              }">
+            </article>
+          `;
+    main.insertAdjacentHTML('beforeend', html);
+  });
+}
+
+export {
+  render
+}
