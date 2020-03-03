8e10f65602f4673d129a2901ae9767642dca9865
deannabosschert <deanna.bosschert@gmail.com>
2020-03-03 13:01:16 +0100

    change render to Render && add Render.detail

--- src/js/modules/render.js
+++ src/js/modules/render.js
@@ -1,73 +1,74 @@
-const render = {
-    overview: function(data) {
-      const view = document.getElementById("overview")
-      const books = data.results
-      const bookList = books.map(book => ({
-        title: book.titles[0],
-        summary: book.summaries,
-        coverImage: book.coverimages[0],
-        detailLink: book.detailLink,
-        authors: book.authors,
-        id: book.id,
-        publisher: book.publisher,
-        genres: book.genres,
-        languages: book.languages,
-        note: book.note
-      }))
+const Render = {
+  overview: function(books) {
+    console.log("rendering overview data")
+    console.log(books)
+    const view = document.getElementById("overview")
+    const bookList = books.map(book => ({
+      title: book.titles[0],
+      summary: book.summaries,
+      coverImage: book.coverimages[0],
+      detailLink: book.detailLink,
+      authors: book.authors,
+      id: book.id,
+      publisher: book.publisher,
+      genres: book.genres,
+      languages: book.languages,
+      note: book.note
+    }))
 
-      const directives = {
-        coverImage: {
-          src: function() {
-            return this.coverImage
-          },
-          href: function() {
-            return this.coverImage
-          }
+    const directives = {
+      coverImage: {
+        src: function() {
+          return this.coverImage
         },
-        detailLink: {
-          href: function() {
-            return this.detailLink
-          }
-        },
-      }
-      Transparency.render(view, bookList, directives)
-    },
-    overview: function(data) {
-      const view = document.getElementById("overview")
-      // const view = document.getElementById(`"${route}"`)
-      const books = data.results
-      const bookList = books.map(book => ({
-        title: book.titles[0],
-        summary: book.summaries,
-        coverImage: book.coverimages[0],
-        detailLink: book.detailLink,
-        authors: book.authors,
-        id: book.id,
-        publisher: book.publisher,
-        genres: book.genres,
-        languages: book.languages,
-        note: book.note
-      }))
-
-      const directives = {
-        coverImage: {
-          src: function() {
-            return this.coverImage
-          },
-          href: function() {
-            return this.coverImage
-          }
-        },
-        detailLink: {
-          href: function() {
-            return this.detailLink
-          }
-        },
-      }
-      Transparency.render(view, bookList, directives)
+        href: function() {
+          return this.coverImage
+        }
+      },
+      detailLink: {
+        href: function() {
+          return this.detailLink
+        }
+      },
     }
+    Transparency.render(view, bookList, directives)
+  },
+  books: function(books) {
+    const view = document.getElementById("overview")
+    // const view = document.getElementById(`"${route}"`)
+    const bookList = books.map(book => ({
+      title: book.titles[0],
+      summary: book.summaries,
+      coverImage: book.coverimages[0],
+      detailLink: book.detailLink,
+      authors: book.authors,
+      id: book.id,
+      publisher: book.publisher,
+      genres: book.genres,
+      languages: book.languages,
+      note: book.note
+    }))
 
-
-    export {
-      render
+    const directives = {
+      coverImage: {
+        src: function() {
+          return this.coverImage
+        },
+        href: function() {
+          return this.coverImage
+        }
+      },
+      detailLink: {
+        href: function() {
+          return this.detailLink
+        }
+      },
     }
+    Transparency.render(view, bookList, directives)
+  }
+}
+
+
+export {
+  Render
+}
