e88a5443fb0f984b84fbcfab69145da61fff0121
deannabosschert <deanna.bosschert@gmail.com>
2020-03-04 01:43:43 +0100

    refactor: " instead of '

--- src/js/modules/API.js
+++ src/js/modules/API.js
@@ -1,51 +1,44 @@
 import {
   Data
 } from "./data.js"
-// public="76f45dfa187d66be5fd6af05573eab04" secret="2cb15758acac08d6ebe6f5ac7a293d69" description="OBA_api_5"
 
-const cors = 'https://cors-anywhere.herokuapp.com/';
-const endpoint = 'https://zoeken.oba.nl/api/v1/search/?';
-const key = '76f45dfa187d66be5fd6af05573eab04';
-const secretKey = '2cb15758acac08d6ebe6f5ac7a293d69';
-const detail = 'Default';
-
+const cors = "https://cors-anywhere.herokuapp.com/"
+const endpoint = "https://zoeken.oba.nl/api/v1/search/?"
+const key = "76f45dfa187d66be5fd6af05573eab04"
+const secretKey = "2cb15758acac08d6ebe6f5ac7a293d69"
+const detail = "Default"
 const config = {
   Authorization: `Bearer ${secretKey}`
-};
+}
 
 const API = {
   get: (route) => {
-    console.log(route)
-
-    function query(route) {
-      if (route === 'overview') {
-        const query = 'q=tolkien'
-        return query
-      } else if (route === 'books') {
-        const query = 'q=tolkien'
-        return query
-      } else {
-        const query = `id=${route}`
-        return query
-      }
-    }
-
-    const queryThing = query(route)
-    console.log(queryThing)
-    const url = `${cors}${endpoint}${queryThing}&authorization=${key}&detaillevel=${detail}&output=json`;
+    const query = getQuery(route)
+    const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`
     fetch(url, config)
       .then(res => {
         return res.json()
       })
       .then(json => {
         const data = json.results
-        console.log(data)
-        console.log(`${route} from API loaded, now storing`)
         Data.store(`${route}`, data)
       })
       .catch(err => {
         console.log(err)
       })
+  }
+}
+
+function getQuery(route) {
+  if (route === "overview") {
+    const query = "q=tolkien"
+    return query
+  } else if (route === "books") {
+    const query = "q=martin"
+    return query
+  } else {
+    const query = `id=${route}`
+    return query
   }
 }
 
--- src/js/modules/routes.js
+++ src/js/modules/routes.js
@@ -6,50 +6,29 @@ import {
 const Routes = {
   handle: () => {
     routie({
-      '': () => {
-        Data.get('overview')
-        updatePageUI('/')
+      "": () => {
+        Data.get("overview")
+        updatePageUI("/")
       },
       books: () => {
-        Data.get('books')
-        updatePageUI('books')
+        Data.get("books")
+        updatePageUI("books")
       },
-      '/:id': id => {
-        // const currentId = JSON.stringify(id)
-        console.log(id)
-        const currentId = id
-        console.log(currentId)
-        // const currentId = this.route
-        // console.log(currentId)
-        // const someId = this.id
-        // console.log(someId)
-        Data.get(currentId)
-        console.log('yeet')
-        updatePageUI('detail')
-      },
-
-      // '/:id': id => {
-      //   const loadData = loadNerds.overview()
-      //   Data.get('detail').then(data => {
-      //     let detail = data.filter(function(data) {
-      //       return data.id == id
-      //     })
-      //     renderNerds.detail(oneNerd)
-      //   })
-      //   updatePageUI('profile')
-      // }
-
+      "/:id": id => {
+        Data.get(id)
+        updatePageUI("detail")
+      }
     })
   }
 }
 
 function updatePageUI(route) {
-  const sections = document.querySelectorAll('section')
+  const sections = document.querySelectorAll("section")
   sections.forEach(section => {
-    section.classList.remove('active')
+    section.classList.remove("active")
   })
   const activeSection = document.querySelector(`[data-route="${route}"]`)
-  activeSection.classList.add('active')
+  activeSection.classList.add("active")
 }
 
 
